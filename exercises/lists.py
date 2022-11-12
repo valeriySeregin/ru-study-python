421eb4584e5f66298f199aa4f4dda2ecfe95d4e4
Valeriy Seregin <valera-s-38@mail.ru>
2022-11-12 18:41:16 +0300

    add solution for lists

--- exercises/lists.py
+++ exercises/lists.py
@@ -1,5 +1,15 @@
 class ListExercise:
     @staticmethod
+    def get_max(input_list: list[int]) -> int:
+        max_item = input_list[0]
+
+        for item in input_list:
+            if item > max_item:
+                max_item = item
+
+        return max_item
+
+    @staticmethod
     def replace(input_list: list[int]) -> list[int]:
         """
         Заменить все положительные элементы целочисленного списка на максимальное значение
@@ -7,7 +17,28 @@ class ListExercise:
         :param input_list: Исходный список
         :return: Список с замененными элементами
         """
-        pass
+        if not input_list:
+            return input_list
+
+        max_item = ListExercise.get_max(input_list)
+
+        processed_list = list(map(lambda num: max_item if num > 0 else num, input_list))
+
+        return processed_list
+
+    @staticmethod
+    def get_search_result(input_list: list[int], query: int, low: int, high: int) -> int:
+        if not input_list or low > high:
+            return -1
+
+        mid = (low + high) // 2
+
+        if input_list[mid] > query:
+            return ListExercise.get_search_result(input_list, query, low, mid - 1)
+        elif input_list[mid] < query:
+            return ListExercise.get_search_result(input_list, query, mid + 1, high)
+        else:
+            return mid
 
     @staticmethod
     def search(input_list: list[int], query: int) -> int:
@@ -18,4 +49,5 @@ class ListExercise:
         :param query: Искомый элемент
         :return: Номер элемента
         """
-        pass
+
+        return ListExercise.get_search_result(input_list, query, 0, len(input_list) - 1)
