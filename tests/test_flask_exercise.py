35b20b8fe5cb68cbfcf394a05c162cd12666896f
Valeriy Seregin <valera-s-38@mail.ru>
2022-11-15 21:05:54 +0300

    add solution for flask

--- tests/test_flask_exercise.py
+++ tests/test_flask_exercise.py
@@ -55,7 +55,7 @@ class TestFlaskExercise:
         )
 
         assert response.status_code == HTTPStatus.UNPROCESSABLE_ENTITY
-        assert response == {"errors": {"name": "This field is required"}}
+        assert response.get_json() == {"errors": {"name": "This field is required"}}
 
     def test_get(self) -> None:
         self.create_user({"name": "Heisenberg"})
