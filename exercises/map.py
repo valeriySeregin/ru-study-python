37dad8b7f828890f8bd5b94730ca98792f93b16d
Valeriy Seregin <valera-s-38@mail.ru>
2022-11-15 21:35:02 +0300

    add solution for map

--- exercises/map.py
+++ exercises/map.py
@@ -12,7 +12,20 @@ class MapExercise:
         Ключи словаря: name, rating_kinopoisk, rating_imdb, genres, year, access_level, country
         :return: Средний рейтинг фильмов у которых две или больше стран
         """
-        pass
+
+        rtn = "rating_kinopoisk"
+        cntr = "country"
+        delimiter = ","
+
+        filtered_movies = filter(
+            lambda movie: len(movie[cntr].split(delimiter)) >= 2, list_of_movies
+        )
+        ratings = map(lambda movie: movie[rtn], filtered_movies)
+        filtered_ratings = filter(lambda rating: rating not in ["", "0"], ratings)
+        ratings_as_floats = map(lambda rating: float(rating), filtered_ratings)
+        floats_as_list = list(ratings_as_floats)
+
+        return sum(floats_as_list) / len(floats_as_list)
 
     @staticmethod
     def chars_count(list_of_movies: list[dict], rating: Union[float, int]) -> int:
@@ -26,4 +39,17 @@ class MapExercise:
         :return: Количество букв 'и' в названиях всех фильмов с рейтингом больше
         или равным заданному значению
         """
-        pass
+
+        rtn = "rating_kinopoisk"
+        name = "name"
+        char_to_count = "и"
+
+        movies_and_ratings = map(
+            lambda movie: {rtn: float(movie[rtn]) if movie[rtn] != "" else 0, name: movie[name]},
+            list_of_movies,
+        )
+        movies_by_rating = filter(lambda movie: movie[rtn] >= rating, movies_and_ratings)
+        char_counts = map(lambda movie: movie[name].count(char_to_count), movies_by_rating)
+        char_counts_as_list = list(char_counts)
+
+        return sum(char_counts_as_list)
